f507d4759a7b3888980126432db37f426fee8d05
Gabriel <gabrielomar2809@gmail.com>
2016-09-25 17:11:52 -0500

    ProjectModified

--- src/ejercicio1/interfaz/Principal.java
+++ src/ejercicio1/interfaz/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio1;
+package ejercicio1.interfaz;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
